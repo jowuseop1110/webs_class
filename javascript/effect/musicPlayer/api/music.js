0330108a314ac89c24d5fb03511facf6c03e52e4
jowunseop1110 <a01083661364@gamil.com>
2022-04-08 14:15:16 +0900

    fdsfds
    fdfsdfd

---
+++ javascript/effect/musicPlayer/api/music.js
@@ -0,0 +1,124 @@
+let allMusic = [
+    {
+        name : "1. 봄여름가을겨울",
+        artist : "빅뱅",
+        img: "music-1",
+        audio: "music01"
+    },
+    {
+        name : "2. INVU",
+        artist : "태연",
+        img: "music-2",
+        audio: "music02"
+    },
+    {
+        name : "3. LOVE DIVE",
+        artist : "아이브",
+        img: "music-3",
+        audio: "music03"
+    },
+    {
+        name : "4. 사랑인가 봐",
+        artist : "멜로망스",
+        img: "music-4",
+        audio: "music04"
+    },
+    {
+        name : "5. GANADARA",
+        artist : "박재범",
+        img: "music-5",
+        audio: "music05"
+    },
+    {
+        name : "6. Feel My Rhythm",
+        artist : "레드벨벳",
+        img: "music-6",
+        audio: "music06"
+    },
+    {
+        name : "7. I NEVER DIE",
+        artist : "아이들",
+        img: "music-7",
+        audio: "music07"
+    },
+    {
+        name : "8. Glitch Mode - The 2nd Album",
+        artist : "NCT DREAM",
+        img: "music-8",
+        audio: "music08"
+    },
+    {
+        name : "9. ELEVEN",
+        artist : "IVE (아이브)",
+        img: "music-9",
+        audio: "music09"
+    },
+    {
+        name : "10. The Feels",
+        artist : "TWICE (트와이스)",
+        img: "music-10",
+        audio: "music10"
+    },
+    {
+        name : "11. Savage - The 1st Mini Album",
+        artist : "aespa",
+        img: "music-11",
+        audio: "music11"
+    },
+    {
+        name : "12. 가리온",
+        artist : "조광일",
+        img: "music-12",
+        audio: "music12"
+    },
+    {
+        name : "13. Levitating (feat. DaBaby)",
+        artist : "Dua Lipa",
+        img: "music-13",
+        audio: "music13"
+    },
+    {
+        name : "14. 쇼미더머니 10-2 Final",
+        artist : "BE'O (비오)",
+        img: "music-14",
+        audio: "music14"
+    },
+    {
+        name : "15. OHAYO MY NIGHT",
+        artist : "디핵 (D-Hack), PATEKO (파테코)",
+        img: "music-15",
+        audio: "music15"
+    },
+    {
+        name : "16. 쇼미더머니 10 Episode 1",
+        artist : "BE'O (비오), 지구인",
+        img: "music-16",
+        audio: "music16"
+    },
+    {
+        name : "17. 미친 소리",
+        artist : "이예준",
+        img: "music-17",
+        audio: "music17"
+    },
+    {
+        name : "18. 쇼미더머니 10 Episode 2",
+        artist : "개코, 조광일; ",
+        img: "music-18",
+        audio: "music18"
+    },
+    {
+        name : "19. 눈이 오잖아(Feat.헤이즈)",
+        artist : "이무진",
+        img: "music-19",
+        audio: "music19"
+    },
+    {
+        name : "20. 연가",
+        artist : "전우성 (노을)",
+        img: "music-20",
+        audio: "music20"
+    },
+  
+
+]
